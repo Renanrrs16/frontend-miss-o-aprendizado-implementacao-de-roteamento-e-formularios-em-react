4adfbc3b0a588d52212b609a43b33a35f8113a29
Renan Souza <133045161+Renanrrs16@users.noreply.github.com>
2024-05-27 18:04:09 +0000

    Api implementada na página de `Create`

--- src/api/api.js
+++ src/api/api.js
@@ -7,12 +7,28 @@ export const Api = {
     },
     readAll: function () {
       return this.endpoint() + '/'
+    },
+    create: function () {
+      return this.endpoint() + '/'
     }
   },
   buildApiGetRequest: function (url) {
-    return fetch(url, { method: 'GET' }).catch(function(error){
+    return fetch(url, { method: 'GET' }).catch(function (error) {
       console.error('Erro ao carregar dados' + url, error)
       toast.error('Erro ao carregar dados.')
     })
+  },
+  buildApiPostRequest: function (url, body) {
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    })
+      .catch(function (error) {
+        console.error('Erro ao enviar dados' + url, error)
+        toast.error('Erro ao enviar dados.')
+      })
   }
 }
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,30 +1,59 @@
-export default function Create(){
+import { toast } from "react-toastify"
+import { Api } from "../api/api"
+import { useNavigate } from "react-router-dom"
+
+export default function Create() {
+  const navigate = useNavigate()
+  
+  async function handleSubmit(event) {
+    event.preventDefault()
+
+    const devmon = {
+      nome: event.target.nome.value,
+      imagem: event.target.imagem.value,
+      evoluiPara: event.target.evoluiPara.value
+    }
+
+    const apiUrl = Api.personagem.create()
+
+    const response = await Api.buildApiPostRequest(apiUrl,devmon)
+
+    if(response.ok){
+      toast.success('Devmon criado com sucesso!')
+      navigate('/')
+    }else{
+      const body = await response.json()
+      toast.error('erro ao criar Devmon: ' + body.error)
+    }
+  }
+
+
   return (
-  <div>
-    <h1>Criar Devmons</h1>
-    
-    <form>
-      <div>
-          <label htmlFor="nome">Nome*:</label><br/>
-          <input type="text" id="nome" name="nome" placeholder="Digite o nome"/>
-      </div>
-      
-      <div>
-          <label htmlFor="imagem">Imagem (URL)*:</label><br/>
-          <input type="text" id="imagem" name="imagem" placeholder="Insira a URL da imagem"/>
-      </div>
+    <div>
+      <h1>Criar Devmons</h1>
 
-      <div>
-          <label htmlFor="evoluiPara">Evolui para (opicional):</label><br/>
-          <input type="text" id="evoluiPara" name="evoluiPara" placeholder="Insira o nome da evolução"/>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="nome">Nome*:</label><br />
+          <input type="text" id="nome" name="nome" placeholder="Digite o nome" />
+        </div>
 
-      <div>
-        <button type="submit">Criar</button>
-        <button type="reset">Reiniciar</button>
-      </div>
+        <div>
+          <label htmlFor="imagem">Imagem (URL)*:</label><br />
+          <input type="text" id="imagem" name="imagem" placeholder="Insira a URL da imagem" />
+        </div>
 
-    </form>
-  </div>
+        <div>
+          <label htmlFor="evoluiPara">Evolui para (opicional):</label><br />
+          <input type="text" id="evoluiPara" name="evoluiPara" placeholder="Insira o nome da evolução" />
+        </div>
+
+        <div>
+          <button type="submit">Criar</button>
+          <button type="reset">Reiniciar</button>
+        </div>
+
+      </form>
+    </div>
   )
 }
