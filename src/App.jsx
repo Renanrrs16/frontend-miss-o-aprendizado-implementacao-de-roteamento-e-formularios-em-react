a6e911cd0722b0dc4f9ed01ca25b4e1b1ca64c8b
Renan Souza <133045161+Renanrrs16@users.noreply.github.com>
2024-05-27 14:58:27 +0000

    Arquivos iniciais do projeto

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import './App.css'
+import Card from './components/Card/Card'
+import { Api } from './api/api'
+
+function App() {
+
+  const [devmons, setDevmons] = useState([])
+
+  async function fetchData() {
+    const apiUrl = Api.personagem.readAll()
+
+    const response = await Api.buildApiGetRequest(apiUrl)
+
+    if(response.ok) {
+      const data = await response.json()  
+
+      setDevmons(data)
+    }else{
+      toast.error('Erro ao carregar lista de Devmon.')
+    }
+
+    
+  }
+
+  useEffect(function () {
+    fetchData()
+  }, [])
+
+
+  return (
+    <>
+
+      <div className='cards'>
+        {devmons.map(function (devmon) {
+          return <Card key={devmon.nome} item={devmon} />
+        })}
+      </div>
+      <ToastContainer/>
+    </>
+  )
+}
+
+export default App
